437b753f3ebfbb835edeee7e13956f1bf080d4a6
WundtAsim <isyangqi@gmail.com>
2021-03-01 21:38:41 +0800

    Revert "New the code for trying"
    This reverts commit 60aedcce09767d378a6109a8a06b14cb78c4c083.

--- Python/fu.py
+++
@@ -1,7 +0,0 @@
-temp = input()
-if temp[0:3] in ['RMB','rmb']:
-    print("USD{:.2f}".format(eval(temp[3:])/6.78))
-elif temp[0:3] in ['USD','usd']:
-    print("RMB{:.2f}".format(eval(temp[3:])*6.78))
-else:
-    print("")
